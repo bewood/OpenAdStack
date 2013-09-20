7c7dc5444a15f56fe3e2a8d311ecbb6505a97b53
bewood <bwoodall@rarecrowds.com>
2013-09-20 15:53:47 -0700

    Build: Update TeamCity Private Build script for git  * Replaces wrapper for java SVN patch submitter  * Pushes current branch to remote path monitored by TeamCity  * See tcpb.cmd help for more information

--- build/tcpb.cmd
+++ build/tcpb.cmd
@@ -1,148 +1,110 @@
 @echo off
 
-::::::::::::::::::::::::::::::
 :: Set local environment
-::::::::::::::::::::::::::::::
 setlocal enabledelayedexpansion
 
-set TCCONFIG=%~dp0..\..\..\LucyConfig\TeamCity
-for /f "tokens=1 delims=" %%i in (%TCCONFIG%\settings.txt) do set %%i
-
-if '%TCCJAR%'=='' set TCCJAR=%~dp0TeamCity\tcc.jar
-if '%MAPPING%'=='' set MAPPING=%~dp0..\.teamcity-mappings.properties
-if '%CONFIGLIST%'=='' set CONFIGLIST=%TCCONFIG%\configurations.txt
-if '%FILELIST%'=='' set FILELIST="%TEMP%\.%RANDOM%.tcdiff"
-
-::::::::::::::::::::::::::::::
 :: Parse arguments
-::::::::::::::::::::::::::::::
 if '%1'=='' goto Help
 
 :ParseArgs
 if not '%1'=='' (
-  if /i '%1'=='Info' set INFO=True
-  if /i '%1'=='Login' set LOGIN=True
-  if /i '%1'=='Logout' set LOGOUT=True
-  if /i '%1'=='Build' set BUILD=True
-  if /i '%1'=='-c' (
-    set CONFIGNAME=%2
+  if /i '%1'=='info' set INFO=true
+  if /i '%1'=='clear' set CLEAR=true
+  if /i '%1'=='build' (
+    set BUILD=true
+    set CONFIG=%2
     shift /1
   )
-  if /i '%1'=='-m' (
-    set MESSAGE=%2
+  if /i '%1'=='-b' (
+    set DEST=%2
     shift /1
   )
-  if '%1'=='/?' goto Help 
-  if '%1'=='-?' goto Help
-  if /i '%1'=='/help' goto Help
-  if /i '%1'=='--help' goto Help
   shift /1
   goto ParseArgs
 )
 
 :: Check that at least one valid function was specified
-if '%INFO%%LOGIN%%LOGOUT%%BUILD%'=='' goto Help
+if '%INFO%%CLEAR%%BUILD%'=='' goto Help
 
-::::::::::::::::::::::::::::::
-:: Find Java
-::::::::::::::::::::::::::::::
-if '%JAVA%'=='' for %%i in (java.exe) do if exist "%%~$PATH:i" set JAVA=%%~$PATH:i
-:: If JAVA not defined and java.exe not found in path, try JAVA_HOME
-if not exist "%JAVA%" if not '%JAVA_HOME%'=='' if exist "%JAVA_HOME%" (
-  if exist "%JAVA_HOME%\bin\java.exe" (
-    set JAVA="%JAVA_HOME%\bin\java.exe"
+set TC.CONFIG=%USERPROFILE%\.openadstack\teamcity.cfg
+
+:: Display saved configuration
+if /i '%INFO%'=='true' (
+  if exist "%TC.CONFIG%" (
+    echo Saved configuration found at "%TC.CONFIG%":
+    type "%TC.CONFIG%"
+    echo.
   ) else (
-    if exist "%JAVA_HOME%\java.exe" set JAVA="%JAVA_HOME%\java.exe"
+    echo No saved configuration found at "%TC.CONFIG%"
   )
 )
-:: Check if java was found
-if not exist "%JAVA%" (
-  echo Error: Unable to locate java. Please add it to your path or set JAVA_HOME.
-  goto End
+
+:: Clear saved configuration
+if /i '%CLEAR%'=='true' (
+  if exist "%TC.CONFIG%" (
+    echo Deleting saved configuration at "%TC.CONFIG%"
+    del /f "%TC.CONFIG%"
+    set TC.USER=
+  ) else (
+    echo No saved configuration found at "%TC.CONFIG%"
+  )    
 )
 
-::::::::::::::::::::::::::::::
-:: Login and logouts
-::::::::::::::::::::::::::::::
-if /i '%INFO%'=='True' (
-	echo   Server: %TEAMCITYURL%
-	echo java.exe: %JAVA%
-	echo  tcc.jar: %TCCJAR%
-	echo.
-)
+:: Submit a personal build to TeamCity via ::ote-run branch
+if /i '%BUILD%'=='true' (
 
-::::::::::::::::::::::::::::::
-:: Login and logouts
-::::::::::::::::::::::::::::::
-if /i '%LOGOUT%'=='True' (
-  %JAVA% -jar %TCCJAR% logout --host %TEAMCITYURL%
-  echo.
-)
-if /i '%LOGIN%'=='True' (
-  %JAVA% -jar %TCCJAR% login --host %TEAMCITYURL% --username %USERNAME%
-  echo.
-)
+  :: Load any saved configuration
+  if exist "%TC.CONFIG%" (
+    for /f "tokens=1 delims=" %%i in (%TC.CONFIG%) do set TC.%%i
+  ) else (
+    if not exist "%USERPROFILE%\.openadstack" mkdir "%USERPROFILE%\.openadstack"
+  )
 
-::::::::::::::::::::::::::::::
-:: Submitting private builds
-::::::::::::::::::::::::::::::
-if /i '%BUILD%'=='True' (
-  :: Check that a message was provided
-  if '%MESSAGE%'=='' goto Help
-
-  :: Get configuration
-  if '%CONFIG%'=='' (
-    if not '%CONFIGNAME%'=='' (
-      :: Find a matching config
-      for /f "tokens=1,2 delims==" %%i in (%CONFIGLIST%) do (
-        if /i '%CONFIGNAME%'=='%%i' (
-          set CONFIG=%%j
-        )
-      )
-    )
-
-    :: Check that a config was found
-    if '!CONFIG!'=='' (
-      if '%CONFIGNAME%'=='' (
-        echo Error: Missing configuration
-      ) else (
-        echo Error: Invalid configuration "%CONFIGNAME%"
-      )
+  :: Prompt for TeamCity user if not loaded from config
+  if '!TC.USER!'=='' (
+    set /p TC.USER=TeamCity User:
+    if '!TC.USER!'=='' (
+      echo TeamCity user name is required.
       echo.
       goto Help
     )
+    echo USER=!TC.USER!>>"%TC.CONFIG%"
   )
 
-  :: Create a list of changed files (excluding unversioned and ignored)
-  if exist %FILELIST% del /q /f %FILELIST%
-  for /f "tokens=1,2" %%i in ('svn st') do if not "%%i"=="?" if not "%%i"=="I" echo %%j >> %FILELIST%
-
-
-  :: Submit the build
-  echo Submitting private build to %TEAMCITYURL% for configuration "%CONFIGNAME%" ^(!CONFIG!^)
-  echo.
-  %JAVA% -jar %TCCJAR% run -c !CONFIG!  -m %MESSAGE% -n %MAPPING% @%FILELIST%
-
-  :: Cleanup the file list
-  del /q /f %FILELIST%
+  :: Submit the push
+  if '%CONFIG%'=='' (
+    echo Please specify a TeamCity build configuration.
+  )
+  if '%DEST%'=='' (
+    for /f "tokens=1,*" %%a in ('git branch') do (
+      if '%%a'=='*' set DEST=%%b
+    )
+  )
+  git push origin +HEAD:remote-run/%CONFIG%/!TC.USER!/!DEST!
 )
 
 goto End
 
-::::::::::::::::::::::::::::::
-:: Help Message
-::::::::::::::::::::::::::::::
 :Help
-echo Submits private builds to TeamCity using the TeamCity java client.
+echo Script to submit TeamCity personal builds using Branch ::ote Run Triggers
+echo %~n0 {info^|build CONFIGURATION [-s SOURCE_BRANCH] [-b DESTINATION_BRANCH]}
+echo info  Display saved settings (if any)
+echo clear Clear any saved settings
+echo build Build the specified configuration
+echo    -b ::ote destination branch (default is same as local active branch)
 echo.
-echo Usage: %~n0 [login ^| logout ^| build -c config -m "message"]
-echo   info          Displays TeamCity server and client information.
-echo   login         Log into TeamCity. Credentials are cached until logout.
-echo   logout        Clear cached credentials. You will need to login again before building.
-echo   build         Submit a private build. Requires -c and -m.
-echo    -m "message" Specifies a message for the private build.
-echo    -c config    Specifies a configuration to build. Must be one of the following:
-for /f "tokens=1 delims==" %%i in (%CONFIGLIST%) do echo                    %%i
+echo This script is simply a wrapper for issuing a 'git push' to a ::ote branch
+echo monitored by a TeamCity Branch ::ote Run Trigger. For example, assuming the
+echo active local branch is 'my_feature', the following invocation:
+echo   %~n0 build bvt
+echo would result in a git push similar to this:
+echo   git push origin +HEAD:::ote-run/bvt/joedeveloper/my_feature
+echo.
+echo The teamcity user name will be prompted for if not already set and then saved
+echo to a per-user configuration file. Use '%~n0 info' to display saved settings.
+echo This script requires corresponding Branch ::ote Run Triggers to be configured
+echo in TeamCity monitoring refs/heads/::ote-run/CONFIGURATION/TEAMCITY_USERNAME/*
+echo.
 
 :End
 endlocal
