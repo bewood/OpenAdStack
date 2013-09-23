212b860177c594f44e2e12ef04b59742fc20eb25
bewood <bwoodall@rarecrowds.com>
2013-09-23 12:39:53 -0700

    AppNexus: Initialize datacosts in E2E integration tests

--- BuildIntegrationClean.cmd
+++ BuildIntegrationClean.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 
-call %~dp0build\buildall.cmd Debug Integration Package NoFxCop NoXmlDoc CleanSql DeploySql CreateAdmin CreateCompany SqlServer .\SQLExpress
+call %~dp0build\buildall.cmd Debug Integration Package NoFxCop NoXmlDoc CleanSql DeploySql CreateAdmin CreateCompany SqlServer .\SQLExpress uploadpersistentdata "%CD%\..\..\LucyConfig\InitialData"
 
 endlocal
